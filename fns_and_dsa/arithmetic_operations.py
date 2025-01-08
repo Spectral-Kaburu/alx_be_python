2f8e08ac912f1a982da22c1f8cb57c2d5f824f35
Lee Kaburu <leekaburu@gmail.com>
2025-01-08 21:39:35 +0300

    edit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,3 +9,14 @@ def perform_operation(num1, num2, operation):
         case "*":
             return num1 * num2
         
+def main():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (+, -, *, /): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
