ba7a93d6b0b3842f1730c757b59b87b606b03ef1
Lee Kaburu <leekaburu@gmail.com>
2025-01-10 10:59:20 +0300

    This Shit is starting to get ridiculous

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,6 +6,7 @@ def display_current_datetime():
 def calculate_future_date():
     days = int(input("Enter the number of days to add to the current date: "))
     future_date = datetime.now() + timedelta(days=days)
+    future_date = future_date.strftime("%Y-%m-%d")
     print(f"Future date : {future_date}")
 
 if __name__ == "__main__":
