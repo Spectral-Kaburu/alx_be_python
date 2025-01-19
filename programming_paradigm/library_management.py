c3809877f9e245094fffd8650bc71c0b2a8f6a6e
Lee Kaburu <spectral@Spectral.Kaburu>
2025-01-19 15:43:23 +0300

    Corrections and modifications

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,34 +2,55 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.__is_checked_out = False
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
 
 
-class Library(Book):
+class Library:
     def __init__(self):
-        self.books = []
-        self.original = []
+        self._books = []
 
     def add_book(self, book):
-        self.books.append(book)
-        self.original.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.books:
+        for book in self._books:
             if book.title == title:
-                self.books.remove(book)
-                self.__is_checked_out = True
-                return book
+                if book.check_out():
+                    print(f"'{title}' has been checked out.")
+                else:
+                    print(f"'{title}' is already checked out.")
+                return
+        print(f"Book titled '{title}' not found in the library.")
 
     def return_book(self, title):
-        for book in self.original:
+        for book in self._books:
             if book.title == title:
-                self.books.append(book)
-                self.__is_checked_out = False
+                if book.return_book():
+                    print(f"'{title}' has been returned.")
+                else:
+                    print(f"'{title}' was not checked out.")
+                return
+        print(f"Book titled '{title}' not found in the library.")
 
     def list_available_books(self):
-        for book in self.books:
-            print(f"{book.title} by {book.author}")
-    
-
-    
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
+        else:
+            print("No books are currently available.")
