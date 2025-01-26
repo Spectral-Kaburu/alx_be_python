d7dd3ee40d5f44ccc254f0048af0adc76a2c7050
Lee Kaburu <leekaburu@gmail.com>
2025-01-26 19:53:33 +0300

    Initial Commit

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,19 @@
+class shape():
+    def area(self):
+        NotImplementedError("The area() method should be overridden in a subclass")
+
+class rectangle(shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14159 * self.radius ** 2
+    
